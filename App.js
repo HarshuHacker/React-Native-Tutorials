a2a2241a15b98079946084f9d2a2cf5456f288ff
Harsh Prasad <harshisindian@gmail.com>
2024-02-18 13:17:54 +0530

    Day 2: Button

--- App.js
+++ App.js
@@ -1,51 +1,30 @@
-import { Image, ImageBackground, ScrollView, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", paddingVertical: 60, paddingHorizontal: 30 }}>
-      <ScrollView>
-        <Image
-          source={{ uri: "https://picsum.photos/200/300" }}
-          style={{ width: 200, height: 200 }}
-        />
-        <Text>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of
-          the printing and typesetting industry. Lorem Ipsum has been the
-          industry's standard dummy text ever since the 1500s, when an unknown
-          printer took a galley of type and scrambled it to make a type specimen
-          book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. It was
-          popularised in the 1960s with the release of Letraset sheets
-          containing Lorem Ipsum passages, and more recently with desktop
-          publishing software like Aldus PageMaker including versions of Lorem
-          Ipsum. Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry's standard
-          dummy text ever since the 1500s, when an unknown printer took a galley
-          of type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. It was popularised in
-          the 1960s with the release of Letraset sheets containing Lorem Ipsum
-          passages, and more recently with desktop publishing software like
-          Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is
-          simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry's standard dummy text ever since the
-          1500s, when an unknown printer took a galley of type and scrambled it
-          to make a type specimen book. It has survived not only five centuries,
-          but also the leap into electronic typesetting, remaining essentially
-          unchanged. It was popularised in the 1960s with the release of
-          Letraset sheets containing Lorem Ipsum passages, and more recently
-          with desktop publishing software like Aldus PageMaker including
-          versions of Lorem Ipsum.
-        </Text>
-      </ScrollView>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "plum",
+        paddingVertical: 60,
+        paddingHorizontal: 30,
+      }}
+    >
+      <Button
+        title="Submit Button"
+        onPress={() => {
+          console.log("Button Pressed");
+        }}
+        color="red"
+        disabled={true}
+      />
     </View>
   );
 }
