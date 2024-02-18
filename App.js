7f1981c72968938744343edfd4638dc12c725f8d
Harsh Prasad <harshisindian@gmail.com>
2024-02-18 13:00:23 +0530

    Day 2: View

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hi Harsh Welcome To Android !!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum" }}>
+      <View style={{ width: 100, height: 100, backgroundColor: "blue" }}></View>
+      <View
+        style={{ width: 100, height: 100, backgroundColor: "green" }}
+      ></View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
