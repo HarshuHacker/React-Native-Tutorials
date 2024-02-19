9e19bd89cc33c08a35da333f19dee8799582523d
Harsh Prasad <harshisindian@gmail.com>
2024-02-19 15:36:10 +0530

    Day 3: Code Setup For Flexbox

--- App.js
+++ App.js
@@ -1,50 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
+import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View
-        style={{ backgroundColor: "black", color: "white", marginBottom: 20 }}
-      >
-        <Text>Hello World</Text>
-      </View>
-      {/* Inheritance Doesn't Works In View To Text */}
-
-      <View style={{ backgroundColor: "black", color: "white" }}>
-        <Text style={{ color: "white" }}>
-          Hello <Text style={{ fontWeight: "bold" }}>World</Text>
-        </Text>
-      </View>
-      {/* Inheritance Works In Text To Text */}
+      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
     </View>
   );
 }
+
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: "plum", padding: 60 },
-  title: { fontSize: 40 },
-  box: {
-    width: 100,
-    height: 100,
-    padding: 10,
-    margin: 20,
-  },
-  lightbluebox: {
-    backgroundColor: "lightblue",
-  },
-  lightgreenbox: {
-    backgroundColor: "lightgreen",
-  },
-  boxShadow: {
-    shadowColor: "red",
-    shadowOffset: {
-      height: 6,
-      width: 6,
-    },
-    shadowOpacity: 0.6,
-    shadowRadius: 4,
-  },
-  androidShadow: {
-    elevation: 10,
-    shadowColor: "red",
+  container: {
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: "red",
   },
 });
