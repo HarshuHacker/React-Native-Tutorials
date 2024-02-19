b1156c09a4a9d94f8ddd49bb7db99c566012521e
Harsh Prasad <harshisindian@gmail.com>
2024-02-19 15:03:06 +0530

    Day 3: Mutiple Styles

--- App.js
+++ App.js
@@ -3,11 +3,27 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>StyleSheet API</Text>
+      <View style={[styles.lightbluebox, styles.box]}>
+        <Text>Lightblue Box</Text>
+      </View>
+      <View style={[styles.lightgreenbox, styles.box]}>
+        <Text>Lightgreen Box</Text>
+      </View>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: { flex: 1, backgroundColor: "plum", padding: 60 },
   title: { fontSize: 40 },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightbluebox: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenbox: {
+    backgroundColor: "lightgreen",
+  },
 });
