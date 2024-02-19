36ab72b57b719b3d1dccfab107765299b3c0dfb2
Harsh Prasad <harshisindian@gmail.com>
2024-02-19 15:40:35 +0530

    Day 3: Flex

--- App.js
+++ App.js
@@ -4,8 +4,8 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "#8e9b00", flex: 1 }}>Box 1</Box>
+      <Box style={{ backgroundColor: "#b65d1f", flex: 4 }}>Box 2</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
       <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
@@ -20,5 +20,6 @@ const styles = StyleSheet.create({
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
+    flex: 1,
   },
 });
