329bd99b9a0d9c1b3c64d7fd51f9ac86846209c4
Harsh Prasad <harshisindian@gmail.com>
2024-02-19 21:49:00 +0530

    Day 3: Flex Wrap

--- App.js
+++ App.js
@@ -4,10 +4,18 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>Box 1</Box>
-      <Box style={{ backgroundColor: "#b65d1f", alignSelf: "flex-end" }}>Box 2</Box>
-      <Box style={{ backgroundColor: "#1c4c56", alignSelf: "center" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "#8e9b00", alignSelf: "flex-start" }}>
+        Box 1
+      </Box>
+      <Box style={{ backgroundColor: "#b65d1f", alignSelf: "flex-end" }}>
+        Box 2
+      </Box>
+      <Box style={{ backgroundColor: "#1c4c56", alignSelf: "center" }}>
+        Box 3
+      </Box>
+      <Box style={{ backgroundColor: "#ab9156", alignSelf: "stretch" }}>
+        Box 4
+      </Box>
       <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
       <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
       <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box>
@@ -20,7 +28,9 @@ const styles = StyleSheet.create({
     marginTop: 64,
     borderWidth: 6,
     borderColor: "red",
-    flex: 1,
+    // flex: 1,
+    height: 600,
+    flexWrap: "wrap",
     // flexDirection: "row",
   },
 });
