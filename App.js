4665ba0c61463c2e969730a651035e5e25807582
Harsh Prasad <harshisindian@gmail.com>
2024-02-20 20:28:10 +0530

    Day 4: useWindowDimensions

--- App.js
+++ App.js
@@ -1,17 +1,32 @@
-import { Dimensions, StyleSheet, Text, View } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  useWindowDimensions,
+} from "react-native";
 
 export default function App() {
+  const windowWidth = useWindowDimensions().width;
+  const windowHeight = useWindowDimensions().height;
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Welcome !!</Text>
+      <View
+        style={[
+          {
+            width: windowWidth > 500 ? "70%" : "90%",
+            height: windowHeight > 600 ? "60%" : "90%",
+          },
+          styles.box,
+        ]}
+      >
+        <Text style={[{ fontSize: windowWidth > 500 ? 50 : 24 }, styles.text]}>
+          Welcome !!
+        </Text>
       </View>
     </View>
   );
 }
-
-const windowWidth = Dimensions.get("window").width
-const windowHeight = Dimensions.get("window").height
 
 const styles = StyleSheet.create({
   container: {
@@ -21,13 +36,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   box: {
-    width: windowWidth > 500 ? "70%" : "90%",
-    height: windowHeight > 600 ? "60%" : "90%",
     backgroundColor: "lightblue",
     alignItems: "center",
     justifyContent: "center",
-  },
-  text: {
-    fontSize: windowWidth > 500 ? 50 : 24,
   },
 });
