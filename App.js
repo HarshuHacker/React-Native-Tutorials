3d9ce894391f460e7086896fea1376102587ad40
Harsh Prasad <harshisindian@gmail.com>
2024-02-21 14:55:10 +0530

    Day 5: FlatList

--- App.js
+++ App.js
@@ -1,50 +1,48 @@
-import { StyleSheet, SafeAreaView, Platform, ScrollView } from "react-native";
-import PokemonCard from "./components/PokemonCard";
+import {
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  FlatList,
+} from "react-native";
+import pokemonList from "./data.json";
 
 export default function App() {
-  const charmanderData = {
-    name: "Charmander",
-    image: require("./assets/charmander.png"),
-    type: "Fire",
-    hp: 39,
-    moves: ["Scratch", "Ember", "Growl", "Leer"],
-    weaknesses: ["Water", "Rock"],
-  };
-
-  const squirtleData = {
-    name: "Squirtle",
-    image: require("./assets/squirtle.png"), // Replace with the actual image path
-    type: "Water",
-    hp: 44,
-    moves: ["Tackle", "Water Gun", "Tail Whip", "Withdraw"],
-    weaknesses: ["Electric", "Grass"],
-  };
-
-  const bulbasaurData = {
-    name: "Bulbasaur",
-    image: require("./assets/bulbasaur.png"), // Replace with the actual image path
-    type: "Grass",
-    hp: 45,
-    moves: ["Tackle", "Vine Whip", "Growl", "Leech Seed"],
-    weaknesses: ["Fire", "Ice", "Flying", "Psychic"],
-  };
-
-  const pikachuData = {
-    name: "Pikachu",
-    image: require("./assets/pikachu.png"), // Replace with the actual image path
-    type: "Electric",
-    hp: 35,
-    moves: ["Quick Attack", "Thunderbolt", "Tail Whip", "Growl"],
-    weaknesses: ["Ground"],
-  };
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        <PokemonCard {...charmanderData} />
-        <PokemonCard {...squirtleData} />
-        <PokemonCard {...bulbasaurData} />
-        <PokemonCard {...pikachuData} />
-      </ScrollView>
+      {/* <ScrollView style={styles.scrollView}>
+        {pokemonList.map((pokemon) => (
+          <View style={styles.card} key={pokemon.id}>
+            <Text style={styles.cardText}>{pokemon.type}</Text>
+            <Text style={styles.cardText}>{pokemon.name}</Text>
+          </View>
+        ))}
+      </ScrollView> */}
+      <View style={styles.scrollView}>
+        <FlatList
+          data={pokemonList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card} key={item.id}>
+                <Text style={styles.cardText}>{item.type}</Text>
+                <Text style={styles.cardText}>{item.name}</Text>
+              </View>
+            );
+          }}
+          keyExtractor={(item) => item.id.toString()}
+          ItemSeparatorComponent={<View style={{ height: 16 }} />}
+          ListEmptyComponent={<Text>No Items Found</Text>}
+          ListHeaderComponent={
+            <Text style={styles.headerText}>Pokemon List</Text>
+          }
+          ListFooterComponent={
+            <Text style={styles.footerText}>End Of List List</Text>
+          }
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -53,6 +51,34 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f5f5f5",
-    paddingTop: Platform.OS === "android" ? 50 : 0,
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+    borderRadius: 8,
+    // marginBottom: 16,
+    borderWidth: 1,
+  },
+  cardText: {
+    fontSize: 30,
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginBottom: 12,
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: "center",
+    marginTop: 12,
+  },
+  sectionHeaderText: {
+    backgroundColor: "#FFFFFF",
+    fontSize: 24,
+    fontWeight: "bold",
   },
 });
