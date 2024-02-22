2820ff43366e20e6c9b18d0c9d4c9b5bc3afa844
Harsh Prasad <harshisindian@gmail.com>
2024-02-22 13:23:17 +0530

    Day 6: Stack Navigator

--- App.js
+++ App.js
@@ -1,207 +1,46 @@
-import { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Button,
-  FlatList,
-  SafeAreaView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import AboutScreen from "./screens/AboutScreen";
+import { Pressable, Text } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [postList, setPostList] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [refreshing, setRefreshing] = useState(false);
-  const [postTitle, setPostTitle] = useState("");
-  const [postBody, setPostBody] = useState("");
-  const [isPosting, setIsPosting] = useState(false);
-  const [error, setError] = useState("");
-
-  const fetchData = async (limit = 10) => {
-    try {
-      const response = await fetch(
-        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
-      );
-      const data = await response.json();
-      setPostList(data);
-      setIsLoading(false);
-      setError("");
-    } catch (error) {
-      console.log("Error In Fetching Data: ", error);
-      setIsLoading(false);
-      setError("Failed To Fetch Post List");
-    }
-  };
-
-  const handleRefresh = () => {
-    setRefreshing(true);
-    fetchData(100);
-    setRefreshing(false);
-  };
-
-  const addPost = async () => {
-    setIsPosting(true);
-    try {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/posts",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            title: postTitle,
-            body: postBody,
-          }),
-        }
-      );
-      const newPost = await response.json();
-      setPostList([newPost, ...postList]);
-      setPostTitle("");
-      setPostBody("");
-      setError("");
-    } catch (error) {
-      console.error("Error adding new post:", error);
-      setError("Failed to add new post.");
-    }
-    setIsPosting(false);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  return isLoading ? (
-    <SafeAreaView style={styles.loadingContainer}>
-      <ActivityIndicator size="large" color="#0000ff" />
-      <Text>Loading !!</Text>
-    </SafeAreaView>
-  ) : (
-    <SafeAreaView style={styles.container}>
-      {error ? (
-        <View style={styles.errorContainer}>
-          <Text style={styles.errorText}>{error}</Text>
-        </View>
-      ) : (
-        <>
-          <View style={styles.inputContainer}>
-            <TextInput
-              style={styles.input}
-              placeholder="Post Title"
-              value={postTitle}
-              onChangeText={setPostTitle}
-            />
-            <TextInput
-              style={styles.input}
-              placeholder="Post Body"
-              value={postBody}
-              onChangeText={setPostBody}
-            />
-            <Button
-              title={isPosting ? "Adding..." : "Add Post"}
-              onPress={addPost}
-              disabled={isPosting}
-            />
-          </View>
-          <View style={styles.listContainer}>
-            <FlatList
-              data={postList}
-              renderItem={({ item }) => {
-                return (
-                  <View style={styles.card}>
-                    <Text style={styles.nameText}>{item.title}</Text>
-                    <Text style={styles.typeText}>{item.body}</Text>
-                  </View>
-                );
-              }}
-              keyExtractor={(item) => item.id.toString()}
-              ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
-              ListEmptyComponent={<Text>No Posts Found</Text>}
-              ListHeaderComponent={
-                <Text style={styles.headerText}>Post List</Text>
-              }
-              ListFooterComponent={
-                <Text style={styles.footerText}>End of list</Text>
-              }
-              refreshing={refreshing}
-              onRefresh={handleRefresh}
-            />
-          </View>
-        </>
-      )}
-    </SafeAreaView>
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#6a51ae" },
+          headerTitleStyle: { fontWeight: "bold" },
+          headerTintColor: "#fff",
+          contentStyle: { backgroundColor: "#e8e4f3" },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Welcome Home",
+            headerStyle: { backgroundColor: "#3a51ae" },
+            headerRight: () => (
+              <Pressable onPress={() => alert("Menu Pressed")}>
+                <Text style={{ color: "#fff", fontSize: 16 }}>Menu</Text>
+              </Pressable>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="About"
+          component={AboutScreen}
+          initialParams={{
+            name: "Guest",
+          }}
+          // options={({ route }) => ({
+          //   title: "About " + route.params.name,
+          // })}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#F5F5F5",
-    paddingTop: StatusBar.currentHeight,
-  },
-  loadingContainer: {
-    flex: 1,
-    backgroundColor: "#F5F5F5",
-    paddingTop: StatusBar.currentHeight,
-    justifyContent: "center", // Center the loading spinner
-    alignItems: "center", // Center the loading spinner
-  },
-  listContainer: {
-    flex: 1,
-    paddingHorizontal: 16,
-  },
-  card: {
-    backgroundColor: "#FFFFFF",
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-  },
-  nameText: {
-    fontSize: 30,
-  },
-  typeText: {
-    fontSize: 24,
-    color: "#666666",
-  },
-  headerText: {
-    fontSize: 24,
-    textAlign: "center",
-    marginBottom: 12,
-  },
-  footerText: {
-    fontSize: 24,
-    textAlign: "center",
-    marginTop: 12,
-  },
-  inputContainer: {
-    backgroundColor: "#FFFFFF",
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    margin: 16,
-  },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 8,
-    padding: 8,
-    borderRadius: 8,
-  },
-  errorContainer: {
-    backgroundColor: "#FFC0CB",
-    padding: 16,
-    borderRadius: 8,
-    borderWidth: 1,
-    margin: 16,
-    alignItems: "center",
-  },
-  errorText: {
-    color: "#D8000C",
-    fontSize: 16,
-    textAlign: "center",
-  },
-});
